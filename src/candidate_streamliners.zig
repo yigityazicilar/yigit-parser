730ce8b4d96de26867d5f405308e591d76d36b53
Orhan Yigit Yazicilar <oyy1@st-andrews.ac.uk>
2024-03-09 01:40:32 +0300

    Might have a working Bin system

--- src/candidate_streamliners.zig
+++ src/candidate_streamliners.zig
@@ -16,28 +16,78 @@ const ModelData = model.ModelData;
 pub const log_level: std.log.Level = .debug;
 
 const DelimiterType = enum { range, op };
-const Op = enum { lower_eq, higher };
+const Op = enum { LEQ, G };
+
 pub const Delimiter = union(DelimiterType) {
-    op: struct { op: Op, value: u32 },
-    range: struct { lower: u8, higher: u8 },
+    op: struct { op: Op, value: isize },
+    range: struct { lower: isize, higher: isize },
 };
 
 pub const BinType = enum { hash, count };
 
 pub const Bin = union(BinType) {
-    hash: std.AutoArrayHashMap(Delimiter, Bin),
+    hash: std.AutoArrayHashMap(Delimiter, *Bin),
     count: u32,
 };
 
-pub fn newBin(allocator: std.mem.Allocator, d: model.Domain) Bin {
-    const domain_bin: Bin = .{
-        .hash = std.AutoArrayHashMap(Delimiter, Bin).init(allocator),
-    };
-
-    const domain_size: u32 = @divFloor(d.domain.upper - d.domain.lower, 10);
-    for (0..10) |i| {
-        print("{} ", .{i});
+pub fn createBin(allocator: std.mem.Allocator, d: model.Domain) *Bin {
+    const domain_bin = try allocator.create(Bin);
+    const list = std.ArrayList(model.Range).init(allocator);
+    for (d.dimensions) |dim| {
+        try list.append(dim);
     }
-    print("{}\n", .{domain_size});
-    return domain_bin;
+
+    try list.append(d.domain);
+
+    return createBinHelper(allocator, domain_bin, list.items);
+}
+
+fn createBinHelper(allocator: std.mem.Allocator, bin: *Bin, ranges: []model.Range) *Bin {
+    if (ranges.len == 0) {
+        bin.count = 0;
+        return bin;
+    }
+
+    bin.hash = std.AutoArrayHashMap(Delimiter, *Bin).init(allocator);
+    const increment: u32 = @divFloor(ranges[0].upper - ranges[0].lower, 10);
+    var l = ranges[0].lower;
+    var u = l + increment;
+    for (0..10) |i| {
+        const d1: Delimiter = .{
+            .range = .{
+                .lower = l,
+                .higher = u,
+            },
+        };
+        const b1: *Bin = try allocator.create(Bin);
+        try bin.hash.put(d1, createBinHelper(allocator, b1, ranges[1..]));
+
+        const d2: Delimiter = .{
+            .op = .{
+                .op = .LEQ,
+                .value = u,
+            },
+        };
+        const b2: *Bin = try allocator.create(Bin);
+        try bin.hash.put(d2, createBinHelper(allocator, b2, ranges[1..]));
+
+        const d3: Delimiter = .{
+            .op = .{
+                .op = .G,
+                .value = l,
+            },
+        };
+        const b3: *Bin = try allocator.create(Bin);
+        try bin.hash.put(d3, createBinHelper(allocator, b3, ranges[1..]));
+
+        if (i == 8) {
+            l = u;
+            u = ranges[0].upper;
+        } else {
+            l = u;
+            u += increment;
+        }
+    }
+
+    return bin;
 }
